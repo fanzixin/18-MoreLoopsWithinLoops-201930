0a5cf4669dbd0397f0f9d78afdf35e48eed20377
fanz1 <fanz1@rose-hulman.edu>
2019-04-28 21:02:54 -0400

    Done

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -4,8 +4,8 @@ in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Zixin Fan.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -50,9 +50,39 @@ def draw_upside_down_wall(rectangle, n, window):
     and n is nonnegative.
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     Some tests are already written for you (above).
     # -------------------------------------------------------------------------
+    rectangle.attach_to(window)
+
+    dx = rectangle.get_width()
+    dy = rectangle.get_height()
+
+    upper_left_x = min(rectangle.corner_1.x, rectangle.corner_2.x)
+    upper_left_y = min(rectangle.corner_1.y, rectangle.corner_2.y)
+    lower_right_x = max(rectangle.corner_1.x, rectangle.corner_2.x)
+    lower_right_y = max(rectangle.corner_1.y, rectangle.corner_2.y)
+
+    for k in range(n - 1):
+        upper_left_x = upper_left_x - dx / 2
+        lower_right_x = lower_right_x - dx / 2
+        upper_left_y = upper_left_y - dy
+        lower_right_y = lower_right_y - dy
+
+        x1 = upper_left_x
+        y1 = upper_left_y
+        x2 = lower_right_x
+        y2 = lower_right_y
+
+        for j in range(k + 2):
+            new_rectangle = rg.Rectangle(rg.Point(x1, y1), rg.Point(x2, y2))
+            new_rectangle.attach_to(window)
+
+            x1 = x1 + dx
+            x2 = x2 + dx
+
+
+    window.render()
 
 
 # -----------------------------------------------------------------------------
